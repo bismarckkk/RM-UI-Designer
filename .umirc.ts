96a7a7d4151931e3ba9a901c5a2529a0326bcd2f
bismarck <zuoqingyu@nuaa.edu.cn>
2023-09-28 23:22:16 +0800

    [fix] 修复了webpack路径错误的问题

--- .umirc.ts
+++ .umirc.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   npmClient: 'pnpm',
   plugins: ['@umijs/plugins/dist/antd'],
   antd: {},
-  base: '/RM-UI-Designer/'
+  base: '/RM-UI-Designer/',
+  publicPath: '/RM-UI-Designer/'
 });
