4645a5db3ce890fd62ec563a771179e3c7e03035
bismarckkk <zuoqingyu@nuaa.edu.cn>
2024-02-17 19:49:18 +0800

    feat: 完成代码生成器Group和Frame

--- src/utils/generator/frame.js
+++ src/utils/generator/frame.js
@@ -1,7 +1,32 @@
-
+import Group from "./group";
 
 class Frame {
+    constructor(name, id, objs) {
+        this.name = name
+        this.id = id
+        this.groups = []
 
+        let _groups = {}
+        for (let obj of objs) {
+            if (obj.group in _groups) {
+                _groups[obj.group].push(obj)
+            } else {
+                _groups[obj.group] = [obj]
+            }
+        }
+
+        for (let group_name in _groups) {
+            this.groups.push(new Group(this.name, this.id, group_name, this.groups.length, _groups[group_name]))
+        }
+    }
+
+    check() {
+        let res = []
+        for (let group of this.groups) {
+            res = res.concat(group.check())
+        }
+        return res
+    }
 }
 
 export default Frame
--- src/utils/generator/group.js
+++ src/utils/generator/group.js
@@ -1,5 +1,42 @@
-class Group {
+import GroupSplit from "./groupSplit";
 
+class Group {
+    constructor(frame_name, frame_id, group_name, group_id, _objs) {
+        this.frame_name = frame_name
+        this.frame_id = frame_id
+        this.group_name = group_name
+        this.group_id = group_id
+        this.splits = []
+
+        let start_id = 0
+        for (let obj of _objs) {
+            if (obj.type === "UiText") {
+                this.splits.push(new GroupSplit(
+                    this.frame_name, this.frame_id, this.group_name, this.group_id,
+                    this.splits.length, start_id, obj
+                ))
+                start_id += 1
+            }
+        }
+
+        const objs = _objs.filter(obj => obj.type !== "UiText");
+        const splitNum = Math.ceil(objs.length / 7);
+        for (let i = 0; i < splitNum; i++) {
+            this.splits.push(new GroupSplit(
+                this.frame_name, this.frame_id, this.group_name, this.group_id,
+                this.splits.length, start_id, objs.slice(i * 7, (i + 1) * 7)
+            ))
+            start_id += 7
+        }
+    }
+
+    check() {
+        let res = []
+        for (let split of this.splits) {
+            res = res.concat(split.check())
+        }
+        return res
+    }
 }
 
 export default Group
