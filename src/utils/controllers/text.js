b8a0d72c052a2047d98c71017b883a181a912bc8
andykong <andy.kong@nuaa.edu.cn>
2024-03-04 20:10:38 +0800

    [feat]新增改变文本scaling和字体大小

--- src/utils/controllers/text.js
+++ src/utils/controllers/text.js
@@ -41,22 +41,23 @@ export const Text = fabric.util.createClass(fabric.Text, {
         this.setControlVisible('mtr', false);
         this.transparentCorners = false;
     },
-    // toObject: function() {
-    //     return {
-    //         type: this.type,
-    //         id: this.id,
-    //         name: this.name,
-    //         layer: this.layer,
-    //         group: this.groupName,
-    //         fontSize: this.fontSize * this.ratio,
-    //         x: this.left * this.ratio,
-    //         y: this.top * this.ratio,
-    //         text: this.text,
-    //         color: this._color,
-    //         team: this.team,
-    //     };
-    // },
-    fromObject: function(object, callback) {
+    toObject: function() {
+        return {
+            type: this.type,
+            id: this.id,
+            name: this.name,
+            layer: this.layer,
+            group: this.groupName,
+            fontSize: this.fontSize * this.ratio,
+            x: this.left * this.ratio,
+            y: this.top * this.ratio,
+            width: this.width * this.ratio,
+            text: this.text,
+            color: this._color,
+            team: this.team,
+        };
+    },
+    fromObject: function(options) {
         this._color = options.color
         this.id = options.id
         this.name = options.name
@@ -65,6 +66,7 @@ export const Text = fabric.util.createClass(fabric.Text, {
         this.set('width', options.width / this.ratio)
         this.set('left', options.x / this.ratio)
         this.set('top', options.y / this.ratio)
+        this.set('fontSize', options.fontSize / this.ratio)
         if (this._color === 'main') {
             this.set('stroke', ColorMap[this.team])
         } else {
@@ -76,8 +78,22 @@ export const Text = fabric.util.createClass(fabric.Text, {
         this.set('width', this.width * this.ratio / ratio)
         this.set('left', this.left * this.ratio / ratio)
         this.set('top', this.top * this.ratio / ratio)
-        this.set('strokeWidth', this.strokeWidth * this.ratio / ratio)
+        this.set('fontSize', this.fontSize * this.ratio / ratio)
         this.ratio = ratio
     },
+    resizeScale: function() {
+        this.set('width', Math.round(this.width * this.scaleX * this.ratio) / this.ratio)
+        this.set('left', Math.round(this.left * this.ratio) / this.ratio)
+        this.set('top', Math.round(this.top * this.ratio) / this.ratio)
+        //this.set('fontSize', Math.round(this.fontSize * this.scaleX * this.ratio) / this.ratio)
+        this.set('scaleX', 1)
+        this.set('scaleY', 1)
+    },
+    setTeam: function (team) {
+        this.team = team
+        if (this._color === 'main') {
+            this.set('stroke', ColorMap[this.team])
+        }
+    }
     // Add other methods and properties as needed
 });
