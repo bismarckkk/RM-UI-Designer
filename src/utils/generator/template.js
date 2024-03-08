6885750e6b4d37c473082ae6d2cb208a4afb681a
bismarckkk <zuoqingyu@nuaa.edu.cn>
2024-03-08 23:20:54 +0800

    fix: 修复了错误的string型代码生成

--- src/utils/generator/template.js
+++ src/utils/generator/template.js
@@ -230,6 +230,7 @@ export function ui_c_string_split(frame_name, frame_id, group_name, group_id,
     const name = obj.name
     const text = obj.text
     obj.color = color2id[obj.color]
+    obj.strLength = text.length
     delete obj.text
     delete obj.type
     delete obj.id
