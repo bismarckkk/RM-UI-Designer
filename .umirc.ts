7b774c573db591861b7da19703473e05dc2be5ea
bismarckkk <zuoqingyu@nuaa.edu.cn>
2024-03-13 17:29:56 +0800

    CI: 添加自动构建nightly版本功能

--- .umirc.ts
+++ .umirc.ts
@@ -2,29 +2,39 @@ import { defineConfig } from "umi";
 
 import fs from 'fs';
 import path from 'path';
+import {GenerateSW} from "workbox-webpack-plugin";
 
 const tauriConf = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'src-tauri/tauri.conf.json'), 'utf-8'));
-const version = `v${tauriConf.package.version}`;
+let version = `v${tauriConf.package.version}`;
+if (process.env.NODE_ENV === 'development') {
+  version = 'development';
+}
+if (process.env.ref) {
+  version = `nightly ${process.env.ref.slice(0, 7)}`;
+}
 
 export default defineConfig({
   npmClient: 'yarn',
   plugins: ['@umijs/plugins/dist/antd'],
   antd: {},
+  publicPath: process.env.ref ? '/nightly/': '/',
   title: 'RM UI Designer',
   history: {type: "hash"},
-  links: [{
+  links: process.env.ref ? [] : [{
     href: '/manifest.json',
     rel: 'manifest'
   }],
   chainWebpack(memo, { webpack } ) {
-    const { GenerateSW } = require("workbox-webpack-plugin");
-    memo.plugin('workbox').use(GenerateSW, [{
-      cacheId: 'rmui-pwa',
-      clientsClaim: true,
-      skipWaiting: true,
-      cleanupOutdatedCaches: true,
-      swDest: 'sw.js'
-    }])
+    if (!process.env.ref) {
+      const { GenerateSW } = require("workbox-webpack-plugin");
+      memo.plugin('workbox').use(GenerateSW, [{
+        cacheId: 'rmui-pwa',
+        clientsClaim: true,
+        skipWaiting: true,
+        cleanupOutdatedCaches: true,
+        swDest: 'sw.js'
+      }])
+    }
     memo.plugin('DefinePlugin').use(webpack.DefinePlugin, [{
       'process.env.VERSION': JSON.stringify(version),
     }])
